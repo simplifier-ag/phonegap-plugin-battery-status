96c65316b9b67f36ea961f951b36b5c82bf44c29
maverickmishra <vishalmishra0907@gmail.com>
2017-04-20 15:07:50 -0700

    Updated manual tests for W3C specs

--- tests/tests.js
+++ tests/tests.js
@@ -444,6 +444,36 @@ exports.defineManualTests = function (contentEl, createActionButton) {
         window.removeEventListener("batterycritical", batteryCritical, false);
     }
 
+    function getChargingChange(){
+        navigator.getBattery().then(function (battery){
+           battery.onchargingchange  = function() {
+                document.getElementById('pluggedValue').innerText = this.charging;
+            };
+        });    
+    }
+    function getChargingTimeChange(){
+        navigator.getBattery().then(function (battery){
+           battery.onchargingchange  = function() {
+                document.getElementById('chrgTimeValue').innerText = this.chargingTime;
+            };
+        });       
+    }
+    // function getdischargingTimeChange(){
+    //     navigator.getBattery().then(function (battery){
+    //         console.log(typeof battery.dischargingTime);
+    //         console.log(battery.dischargingTime);
+    //        battery.ondischargingtimechange  = function() {
+    //             document.getElementById('dischrgTimeValue').innerText = this.dischargingTime;
+    //         };
+    //     });        
+    // }
+    function getChargingLevel(){
+        navigator.getBattery().then(function (battery){         
+           battery.onlevelchange = function() {
+                document.getElementById('levelValue').innerText = this.level;
+            };
+        });        
+    }
     //Generate Dynamic Table
     function generateTable(tableId, rows, cells, elements) {
         var table = document.createElement('table');
@@ -557,6 +587,40 @@ exports.defineManualTests = function (contentEl, createActionButton) {
                 cell : 1
             }
         }
+        , {
+            id : "chrgTimeTag",
+            content : "Charging Time:",
+            tag : "div",
+            position : {
+                row : 5,
+                cell : 0
+            }
+        }, {
+            id : "chrgTimeValue",
+            content : "",
+            tag : "div",
+            position : {
+                row : 5,
+                cell : 1
+            }
+        }, 
+        // {
+        //     id : "dischrgTimeTag",
+        //     content : "Discharging Time:",
+        //     tag : "div",
+        //     position : {
+        //         row : 6,
+        //         cell : 0
+        //     }
+        // }, {
+        //     id : "dischrgTimeValue",
+        //     content : "",
+        //     tag : "div",
+        //     position : {
+        //         row : 6,
+        //         cell : 1
+        //     }
+        // }
     ];
 
     //Title audio results
@@ -565,7 +629,7 @@ exports.defineManualTests = function (contentEl, createActionButton) {
     div.setAttribute("align", "center");
     contentEl.appendChild(div);
 
-    var batteryTable = generateTable('info', 5, 3, batteryElements);
+    var batteryTable = generateTable('info', 7, 3, batteryElements);
     contentEl.appendChild(batteryTable);
 
     div = document.createElement('h2');
@@ -581,8 +645,15 @@ exports.defineManualTests = function (contentEl, createActionButton) {
         '<div id="addBl"></div><div id="remBl"></div>' +
         '<h3>Battery Critical Tests</h3>' +
         '</p> Will update value for battery critical to true when battery is below 5%' +
-        '<div id="addBc"></div><div id="remBc"></div>';
-
+        '<div id="addBc"></div><div id="remBc"></div>'+
+        '</p> Will update value for charging status' +
+        '<div id="chckChargingChange"></div>'+
+        '</p> Will update value for charge level' +
+        '<div id="chckChargingLevel"></div>'+
+        '</p> Will update value for charging time' +
+        '<div id="chckChargingTime"></div>';
+        // '</p> Will update value for discharging time' +
+        // '<div id="chckdischargingTime"></div>';
     createActionButton('Add "batterystatus" listener', function () {
         addBattery();
     }, 'addBS');
@@ -601,4 +672,18 @@ exports.defineManualTests = function (contentEl, createActionButton) {
     createActionButton('Remove "batterycritical" listener', function () {
         removeCritical();
     }, 'remBc');
-};
+    createActionButton('check charging change event', function () {
+        getChargingChange();
+    }, 'chckChargingChange');
+    createActionButton('check charging level event', function () {
+        getChargingLevel();
+    }, 'chckChargingLevel');
+    createActionButton('check charging time change event', function () {
+        getChargingTimeChange();
+    }, 'chckChargingTime');
+    // createActionButton('check discharging time change event', function () {
+    //     getdischargingTimeChange();
+    // }, 'chckdischargingTime');
+
+
+};  
